5974290f413efc3a56f7ce57e5398a9cd53b97e4
dhirtiman <dhirtimankhati@gmail.com>
2026-04-16 19:54:25 +0530

    codes

--- preSessional/6.c
+++ preSessional/6.c
@@ -5,6 +5,39 @@
 #include <conio.h>
 #include <string.h>
 
+/*
+include io and io and string.h not strings.h
+
+declare 4 char arrays of arbritary sizes,
+str1,str2,str3,sub
+ two ints, pos and len 
+
+input str1 and str2 using, gets(string)
+
+1. demo string copying: 
+  copy str1 to str3(undefined) 
+  using, strcpy(str3,str1);
+  print str3 using %s
+3. demo string comparison:
+  check if str3 and str2 are equal
+   using strcmp(str3,str2) -> res
+    if res = 0 , they are equal
+	else not equal 
+4.  substring extraction
+  input starting postion and length of the sub string
+  in pos and len
+
+  extract substring into sub
+  using strcpy(sub,str3+pos-1,len): , 
+  str3+pos-1 means treat the str3 string from pos-1 index
+  eg:  str3 = "bookworm" , pos = 5 , len = 4 ,
+  so -> str3+pos-1 = "worm" sub = "worm"
+
+  set sub[len] = '\0' 
+
+
+*/
+
 void main()
 {
 	char str1[50], str2[50], str3[50];
