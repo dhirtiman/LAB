4bb548682b3b1ed81d7ae66a56b5983edd0e24b7
dhirtiman <dhirtimankhati@gmail.com>
2026-04-16 20:04:32 +0530

    codes

--- preSessional/7.c
+++ preSessional/7.c
@@ -5,6 +5,31 @@
 #include <conio.h>
 #include <string.h>
 
+/*
+include io and io andd string.h
+
+declare two char arrays , 
+two iterators for each array ,
+two size store for each array 
+one flag to check if found
+
+input two str using gets(str) , text,pattern
+extract strings lengths using 
+  strlen(string)
+
+iterate  i from 0  to texlen-patlen 
+   iterate j from 0 to patlen-1 
+    check if(text[i+j] != pattern[j]): meaaning check if any of the characters dont match
+       yes, break
+    check if j == patlen-1 : we were abke to traverse whole pattern without interuption
+     yes, print pattern found at index i , set flag = 1
+
+
+if(flag=0):
+  print pattern was not found anywhere
+
+*/
+
 void main()
 {
     char text[100], pattern[50];
